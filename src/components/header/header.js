ff5677f9a3d7c6cf5b6e765e56b598b49e22e7b7
Shabedum <gergaulovsiti@gmail.com>
2024-05-17 19:59:18 +0300

    header wrap

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,17 @@
 mainObj.header =
 {
+	headerWrap:document.querySelector('._headerWrap'),
 	show(instance)
 	{
 		if(instance.classList.contains('active'))
 		{
 			instance.classList.remove('active')
+			this.headerWrap.classList.remove('active')
 		}
 		else
 		{
 			instance.classList.add('active')
+			this.headerWrap.classList.add('active')
 		}
 	}
 }
